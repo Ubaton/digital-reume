a445ce62517c4cb231ba07a4a942650574ab1b77
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-01-08 17:56:54 +0200

    first commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+        <Link to="/experience" className="[&.active]:font-bold">
+          Experience
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  ),
+});
