0e039d822414a3b322c61797b51abf7df2b6c443
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-01-09 08:55:41 +0200

    contact infor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: FC = () => {
     <>
       <nav className="bg-zinc-50 p-4 rounded-2xl border-b border-zinc-500">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-zinc-700 text-lg font-bold">Raymond Ngobeni</div>
+          <div className="text-zinc-700 text-lg font-bold"><Link to="/">Raymond Ngobeni</Link></div>
           <div className="flex space-x-4">
             {links.map((item, index) => (
               <Link
--- src/components/sections/contact-infor.tsx
+++ src/components/sections/contact-infor.tsx
@@ -1,24 +1,42 @@
-import {FC} from 'react'
+import { Envelope, Telephone, At, ExternalLink } from "@mynaui/icons-react";
+import { FC } from "react";
 
 interface ContactProps {
-  contact: Number;
-  email: String;
-  address: String;
-  website: String
+  icon: React.ComponentType;
+  name: string;
+  value: string;
 }
 
 const information: ContactProps[] = [
-  {icon: #, label: "Contact", name: ""}
-]
+  { icon: Telephone, name: "Contact", value: "0612002807" },
+  { icon: Envelope, name: "Email", value: "mrrngobeni6@gmail.com" },
+  {
+    icon: At,
+    name: "Address",
+    value: "4912 De Bruyn Street, Chloorkop, Edenvale, South Africa, 1620",
+  },
+  {
+    icon: ExternalLink,
+    name: "Website",
+    value: "https://raymond-ngobeni-protfolio.vercel.app/",
+  },
+];
 
 const ContactInfor: FC = () => {
   return (
     <header>
       <div>
-
+        {information.map((info, index) => (
+          <div key={index} className="flex items-center space-x-2">
+            <info.icon />
+            <span>
+              {info.name}: {info.value}
+            </span>
+          </div>
+        ))}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default ContactInfor
+export default ContactInfor;
