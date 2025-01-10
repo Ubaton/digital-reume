75e9a6b6f9dd6d8c3eafd80cd3195a53d985898b
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-01-10 18:42:31 +0200

    icons added error test passed

--- src/components/sections/contact-infor.tsx
+++ src/components/sections/contact-infor.tsx
@@ -1,53 +1,75 @@
 import { FC } from "react";
-
-
+import { MdAlternateEmail, MdOutlineEmail } from "react-icons/md";
+import { PiPhoneCall } from "react-icons/pi";
+import { FiLink2 } from "react-icons/fi";
 
 interface ContactProps {
-  // icon: React.ComponentType;
+  icon: React.ComponentType;
   name: string;
   value: string;
 }
 
+interface BioProps {
+  title: string;
+  description: string;
+}
+
 const information: ContactProps[] = [
   {
-    // icon: ,
+    icon: PiPhoneCall,
     name: "Contact",
     value: "0612002807",
   },
   {
-    // icon: ,
+    icon: MdOutlineEmail,
     name: "Email",
     value: "mrrngobeni6@gmail.com",
   },
   {
-    // icon: ,
+    icon: MdAlternateEmail,
     name: "Address",
     value: "4912 De Bruyn Street, Chloorkop, Edenvale, South Africa, 1620",
   },
   {
-    // icon: ,
+    icon: FiLink2 ,
     name: "Website",
     value: "https://raymond-ngobeni-protfolio.vercel.app/",
+  },
+];
+
+const bio: BioProps[] = [
+  {
+    title: "Summary",
+    description: `
+      As a skilled full-stack developer, I am dedicated to turning ideas 
+            into innovative web applications. Explore my latest projects and 
+            articles, showcasing my expertise in React JS, Next JS, React 
+            Native, and Web development.
+    `,
   },
 ];
 
 const ContactInfor: FC = () => {
   return (
     <header>
-   
-      <div>
-        {information.map((info, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-center space-x-2"
-          >
-            {/* <info.icon /> */}
-            <span>
-              {info.name}: {info.value}
-            </span>
-            
-          </div>
-        ))}
+      <div className="flex justify-between container">
+        <div>
+          {information.map((info, index) => (
+            <div key={index} className="flex  space-x-2">
+              <info.icon />
+              <span>
+                {info.name}: {info.value}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div>
+          {bio.map((bioinfo, index) => (
+            <div key={index}>
+              <strong>{bioinfo.title}:</strong> {bioinfo.description}
+            </div>
+          ))}
+        </div>
       </div>
     </header>
   );
